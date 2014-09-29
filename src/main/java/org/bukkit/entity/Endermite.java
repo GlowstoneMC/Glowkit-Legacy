5db9883d8835de33b7ae6dad10af905753e81979
Momo <aramax2000@hotmail.com>
2014-09-29 08:56:11 -0400

    Add lifetime of Endermite.

--- src/main/java/org/bukkit/entity/Endermite.java
+++ src/main/java/org/bukkit/entity/Endermite.java
@@ -3,4 +3,10 @@ package org.bukkit.entity;
 /**
  * Represents an Endermite
  */
-public interface Endermite extends Monster {}
+public interface Endermite extends Monster {
+    /**
+     * Represents the lifetime in ticks of the Endermite. Should disappear after 2400 ticks.
+     * @return the lifetime of the Endermite in ticks.
+     */
+    public int getLifetime();
+}
