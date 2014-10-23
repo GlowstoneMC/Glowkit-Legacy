e0136c0099d53d48a82db38564bf8071c7646374
Alex Spieksma <alexspieksma@gmail.com>
2014-10-23 23:17:16 +0200

    Deprecate old flower pot contents functions

--- src/main/java/org/bukkit/material/FlowerPot.java
+++ src/main/java/org/bukkit/material/FlowerPot.java
@@ -52,7 +52,9 @@ public class FlowerPot extends MaterialData {
      *
      * @return material MaterialData for the block currently in the flower pot
      *     or null if empty
+     * @deprecated Use {@link org.bukkit.block.FlowerPot#getContents()} instead.
      */
+    @Deprecated
     public MaterialData getContents() {
         switch (getData()) {
             case 1:
@@ -86,7 +88,9 @@ public class FlowerPot extends MaterialData {
      * Set the contents of the flower pot
      *
      * @param materialData MaterialData of the block to put in the flower pot.
+     * @deprecated Use {@link org.bukkit.block.FlowerPot#setContents(MaterialData)} instead.
      */
+    @Deprecated
     public void setContents(MaterialData materialData) {
         Material mat = materialData.getItemType();
 
